4be25452578735949e12f2b8c939d0b95b4b03a5
djatkdgus789 <djatkdgus789@unist.ac.kr>
2023-01-10 11:10:30 +0000

    add template-start-exp code

---
+++ exp-template/cold_start_language_runtime_template_sandbox.sh
@@ -0,0 +1,51 @@
+#/bin/bash
+index=1
+
+rootfs_path="/mnt/nvme1/rootfs/debian-rootfs.ext4"
+arch=`uname -m`
+kernel_path="/home/ssl/vanilla-kernel/vmlinux"
+socket_path=/tmp/firecracker-${index}.socket 
+snapshot_path=/mnt/nvme1/snapshot/language-runtime-template-sandbox/snapshot_file
+mem_file_path=/mnt/nvme1/snapshot/language-runtime-template-sandbox/mem_file
+
+sync;
+echo 3 > /proc/sys/vm/drop_caches 
+
+echo "load snapshot"
+curl --unix-socket $socket_path -i \
+    -X PUT "http://localhost/snapshot/load" \
+    -H "accept: application/json" \
+    -H "Content-Type: application/json" \
+    -d "{
+    	\"snapshot_path\": \"${snapshot_path}\",
+       	\"mem_file_path\":\"${mem_file_path}\",
+	\"enable_diff_snapshots\": true,
+	\"resume_vm\": false
+    }"
+
+echo "resume VM"
+curl --unix-socket $socket_path -i \
+    -X PATCH 'http://localhost/vm' \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "state": "Resumed"
+}'
+
+echo "\nRecognition invocation"
+sudo curl --max-time 100 --request POST 'http://192.168.0.3:5000/invoke?function=image&redishost=10.20.18.215&redispasswd=s$l0407' \
+	--header 'Content-Type: application/json' \
+	--data-raw '{
+	"model_object_key":"resnet50-19c8e357.pth",
+    	"input_object_key": "100kb.jpg",
+        "output_object_key_prefix": "outputimg-"
+}'
+
+echo "graceful leaving"
+curl --unix-socket $socket_path -i \
+    -X PUT 'http://localhost/actions'       \
+    -H  'Accept: application/json'          \
+    -H  'Content-Type: application/json'    \
+    -d '{
+    "action_type": "SendCtrlAltDel"
+}'
--- exp-template/launch_vmm.sh
+++ exp-template/launch_vmm.sh
@@ -14,7 +14,7 @@ sudo killall -9 firecracker
 sudo setfacl -m u:${USER}:rw /dev/kvm
 sudo rm -f /tmp/firecracker*.socket
 
-export FC_PATH="$HOME/firecracker-1.0.0/build/cargo_target/x86_64-unknown-linux-musl/debug/"
+export FC_PATH="/home/ssl/Serverless-Benchmark/firecracker-1.0.0/build/cargo_target/x86_64-unknown-linux-musl/debug/"
 
 for var in `seq 1 ${num_vm}`; do
 	sudo /bin/ip netns exec fc$var $FC_PATH/firecracker --api-sock /tmp/firecracker-$var.socket &
